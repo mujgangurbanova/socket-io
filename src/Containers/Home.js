6420c95f022be814b613fed3d8330ef1fea80406
Eldar Babayev <eldar.babayev@yahoo.com>
2021-08-23 20:24:47 +0400

    fix

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,62 +1,63 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import Profile from "../components/Profile";
-import Chat from "../components/Chat";
-
 import "../App.css";
+
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import socket from "../socket";
+
+import Chat from "../components/Chat";
+import Profile from "../components/Profile";
 import { receiveMessageAction } from "../redux/actionCreators";
+import socket from "../socket";
+import styled from "styled-components";
 
 function Home() {
-  const [activeConversation, setActiveConversation] = useState("");
-  const messages = useSelector((state) => state.messages[activeConversation]);
-  const dispatch = useDispatch();
+	const [activeConversation, setActiveConversation] = useState({});
+	const messages = useSelector((state) => state.messages[activeConversation]);
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    socket.on("receive message", (message, username) => {
-      console.log("recevied", message, username);
-      dispatch(receiveMessageAction(username, message));
-    });
+	useEffect(() => {
+		socket.on("receive message", (message, username) => {
+			console.log("recevied", message, username);
+			dispatch(receiveMessageAction(username, message));
+		});
 
-    return () => {
-      socket.off("send message");
-    };
-    // eslint-disable-next-line
-  }, []);
+		return () => {
+			socket.off("send message");
+		};
+		// eslint-disable-next-line
+	}, []);
 
-  const handleConversationClick = (username) => {
-    setActiveConversation(username);
-  };
+	const handleConversationClick = (username, id) => {
+		setActiveConversation({ username, id });
+	};
 
-  return (
-    <Page>
-      <Box>
-        <Profile onConversationClick={handleConversationClick} />
-        {activeConversation ? (
-          <Chat toUsername={activeConversation} messages={messages} />
-        ) : (
-          <span>Select a conversation to start chatting</span>
-        )}
-      </Box>
-    </Page>
-  );
+	return (
+		<Page>
+			<Box>
+				<Profile onConversationClick={handleConversationClick} />
+				{activeConversation ? (
+					<Chat toUsername={activeConversation} messages={messages} />
+				) : (
+					<span>Select a conversation to start chatting</span>
+				)}
+			</Box>
+		</Page>
+	);
 }
 
 const Page = styled.div`
-  display: flex;
-  height: 100vh;
-  align-items: center;
-  justify-content: center;
-  background-color: white;
+	display: flex;
+	height: 100vh;
+	align-items: center;
+	justify-content: center;
+	background-color: white;
 `;
 
 const Box = styled.div`
-  height: 90%;
-  width: 90%;
-  background-color: white;
-  border-radius: 20px;
-  display: flex;
+	height: 90%;
+	width: 90%;
+	background-color: white;
+	border-radius: 20px;
+	display: flex;
 `;
 
 export default Home;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,160 +1,161 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import styled from "styled-components";
 import {
-  addNewConversationAction,
-  setConversationsAction,
+	addNewConversationAction,
+	setConversationsAction,
 } from "../redux/actionCreators";
-import PersonInformation from "./PersonInformation";
-import eli from "../images/eli.jpg";
+import { useDispatch, useSelector } from "react-redux";
+
 import { MyImage } from "./Chat";
-import socket from "../socket";
+import PersonInformation from "./PersonInformation";
 import axios from "../axios";
+import eli from "../images/eli.jpg";
+import socket from "../socket";
+import styled from "styled-components";
 
 const Profile = ({ onConversationClick }) => {
-  const [isClicked, setIsClicked] = useState(0);
-  const [isOpen, setIsOpen] = useState(false);
-  const dispatch = useDispatch();
-  const conversations = useSelector((state) => state.conversations);
-  const currentUser = useSelector((state) => state.currentUser);
+	const [isClicked, setIsClicked] = useState(0);
+	const [isOpen, setIsOpen] = useState(false);
+	const dispatch = useDispatch();
+	const conversations = useSelector((state) => state.conversations);
+	const currentUser = useSelector((state) => state.currentUser);
 
-  useEffect(() => {
-    socket.on("new user", (user) => {
-      dispatch(addNewConversationAction(user));
-    });
+	useEffect(() => {
+		socket.on("new user", (user) => {
+			dispatch(addNewConversationAction(user));
+		});
 
-    axios.get("users").then((response) => {
-      const users = response.data.filter(
-        (user) => user.username !== currentUser.username
-      );
-      dispatch(setConversationsAction(users));
-    });
+		axios.get("users").then((response) => {
+			const users = response.data.filter(
+				(user) => user.username !== currentUser.username
+			);
+			dispatch(setConversationsAction(users));
+		});
 
-    return () => {
-      socket.off("new user");
-    };
-    // eslint-disable-next-line
-  }, []);
+		return () => {
+			socket.off("new user");
+		};
+		// eslint-disable-next-line
+	}, []);
 
-  const handleClick = () => {
-    if (isOpen) {
-      setIsOpen(false);
-    } else {
-      setIsOpen(true);
-    }
-  };
+	const handleClick = () => {
+		if (isOpen) {
+			setIsOpen(false);
+		} else {
+			setIsOpen(true);
+		}
+	};
 
-  return (
-    <Personal>
-      <div className="quick-chat">
-        <i className="fab fa-facebook-messenger"></i>
-        <h2>QuickChat</h2>
-      </div>
-      <PersonInformation />
-      <ActiveConversations>
-        <div className="active-talk">
-          <h3>Active Conversations</h3>
-          <span>{conversations.length}</span>
-          <i
-            onClick={handleClick}
-            className={isOpen ? "fas fa-chevron-up" : "fas fa-chevron-down"}
-          ></i>
-        </div>
-        <PersonList>
-          {conversations.map((user) => (
-            <Persons
-              onClick={() => onConversationClick(user.username)}
-              key={user.username}
-              isOpen={isOpen}
-            >
-              <MyImage src={eli} />
-              <h3>{user.username}</h3>
-            </Persons>
-          ))}
-        </PersonList>
-      </ActiveConversations>
-    </Personal>
-  );
+	return (
+		<Personal>
+			<div className="quick-chat">
+				<i className="fab fa-facebook-messenger"></i>
+				<h2>QuickChat</h2>
+			</div>
+			<PersonInformation />
+			<ActiveConversations>
+				<div className="active-talk">
+					<h3>Active Conversations</h3>
+					<span>{conversations.length}</span>
+					<i
+						onClick={handleClick}
+						className={isOpen ? "fas fa-chevron-up" : "fas fa-chevron-down"}
+					></i>
+				</div>
+				<PersonList>
+					{conversations.map((user) => (
+						<Persons
+							onClick={() => onConversationClick(user.username, user.id)}
+							key={user.username}
+							isOpen={isOpen}
+						>
+							<MyImage src={eli} />
+							<h3>{user.username}</h3>
+						</Persons>
+					))}
+				</PersonList>
+			</ActiveConversations>
+		</Personal>
+	);
 };
 
 export default Profile;
 
 export const Personal = styled.div`
-  width: 30%;
-  height: auto;
-  display: flex;
-  flex-direction: column;
-  .quick-chat {
-    display: flex;
-    align-items: center;
-    margin-bottom: 10px;
-    h2 {
-      font-size: 21px;
-      margin: 5px 0 13px 0;
-    }
+	width: 30%;
+	height: auto;
+	display: flex;
+	flex-direction: column;
+	.quick-chat {
+		display: flex;
+		align-items: center;
+		margin-bottom: 10px;
+		h2 {
+			font-size: 21px;
+			margin: 5px 0 13px 0;
+		}
 
-    i {
-      font-size: 31px;
-      color: #7c6bc7;
-      margin-right: 10px;
-    }
-  }
+		i {
+			font-size: 31px;
+			color: #7c6bc7;
+			margin-right: 10px;
+		}
+	}
 `;
 const ActiveConversations = styled.div`
-  height: 50%;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-top: 20px;
-  .active-talk {
-    display: flex;
-    align-items: center;
-    position: relative;
-    h3 {
-      margin: 0;
-    }
+	height: 50%;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	margin-top: 20px;
+	.active-talk {
+		display: flex;
+		align-items: center;
+		position: relative;
+		h3 {
+			margin: 0;
+		}
 
-    i {
-      position: absolute;
-      right: -53px;
-      cursor: pointer;
-    }
-    span {
-      background-color: #ebedee;
-      font-weight: bold;
-      border-radius: 50%;
-      padding: 5px 9px;
-      margin-left: 7px;
-    }
-  }
+		i {
+			position: absolute;
+			right: -53px;
+			cursor: pointer;
+		}
+		span {
+			background-color: #ebedee;
+			font-weight: bold;
+			border-radius: 50%;
+			padding: 5px 9px;
+			margin-left: 7px;
+		}
+	}
 `;
 
 const PersonList = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 10px;
-  width: 73%;
+	display: flex;
+	flex-direction: column;
+	margin-top: 10px;
+	width: 73%;
 `;
 
 const Persons = styled.div`
-  display: flex;
-  margin-bottom: 10px;
-  align-items: center;
-  cursor: pointer;
-  opacity: ${({ isOpen }) => (isOpen ? "1" : "0")};
-  height: ${({ isOpen }) => (isOpen ? "auto" : "0")};
-  margin: ${({ isOpen }) => (isOpen ? "10px 0" : "0")};
-  transform: ${({ isOpen }) =>
-    isOpen ? "translateY(0)" : "translateY(-50px)"};
-  transition: all 0.3s linear;
-  padding: 0 8px;
+	display: flex;
+	margin-bottom: 10px;
+	align-items: center;
+	cursor: pointer;
+	opacity: ${({ isOpen }) => (isOpen ? "1" : "0")};
+	height: ${({ isOpen }) => (isOpen ? "auto" : "0")};
+	margin: ${({ isOpen }) => (isOpen ? "10px 0" : "0")};
+	transform: ${({ isOpen }) =>
+		isOpen ? "translateY(0)" : "translateY(-50px)"};
+	transition: all 0.3s linear;
+	padding: 0 8px;
 
-  h3 {
-    margin: 0 0 0 10px;
-    color: #394f60;
-  }
-  &:hover {
-    background-color: #f2f6fc;
-    border-radius: 8px;
-  }
+	h3 {
+		margin: 0 0 0 10px;
+		color: #394f60;
+	}
+	&:hover {
+		background-color: #f2f6fc;
+		border-radius: 8px;
+	}
 `;
